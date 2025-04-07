796a2927b37d3d434b135e9635a2b0eba2277c24
Juan Guerrero <juanpabloguleal@gmail.com>
2025-04-07 00:02:13 -0600

    SQS Consumer

---
+++ 1.Sqs/SQS/SQS.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+CancellationTokenSource cts = new CancellationTokenSource();
+AmazonSQSClient client = new AmazonSQSClient();
+
+var queueUrlResponse = await client.GetQueueUrlAsync("customers");
+
+ReceiveMessageRequest request = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+};
+
+while (!cts.IsCancellationRequested)
+{
+    ReceiveMessageResponse response = await client.ReceiveMessageAsync(request, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await client.DeleteMessageAsync(request.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
