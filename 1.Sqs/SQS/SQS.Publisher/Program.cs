f0af6394adec757a6d6378d393149a7f7d56507c
Juan Guerrero <juanpabloguleal@gmail.com>
2025-04-06 23:52:40 -0600

    SQS Publisher

---
+++ 1.Sqs/SQS/SQS.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SQS.Publisher;
+
+AmazonSQSClient client = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "juanpabloguleal@gmail.com",
+    FullName = "Juan Guerrero",
+    DateOfBirth = new DateTime(1997,12,20)
+};
+
+var queueUrlResponse = await client.GetQueueUrlAsync("customers");
+
+
+var sendMessageRequest = new SendMessageRequest()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+    {
+        {
+            "MessageType", new MessageAttributeValue() { DataType = "String", StringValue = nameof(CustomerCreated) },
+
+        }
+    }
+};
+
+var response = client.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
